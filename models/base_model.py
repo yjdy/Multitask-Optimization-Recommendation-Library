f1f504781d15265ce75fcbcf1458ca8f02ce91ea
yjdy <yuanjunfy@163.com>
2024-04-11 11:40:01 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,40 @@
 import torch
 from torch import nn
 
+from .layers import EmbeddingLayer, MultiLayerPerceptron
+
 class AddGaussianNoise:
-    def __init__(self, mean=0.0, std=0.1):
+    def __init__(self, mean=0.0, std=0.1,**kwargs):
         self.mean = mean
         self.std = std
 
     def __call__(self, tensor):
         noise = torch.randn(tensor.size()) * self.std + self.mean
-        noisy_tensor = tensor + noise
+        noisy_tensor = tensor + noise.to(tensor.device)
         return noisy_tensor
 
 
 class AddUniformNoise:
-    def __init__(self, std=1):
+    def __init__(self, std=1,**kwargs):
         self.std = std
 
     def __call__(self, tensor):
         noise = (torch.rand_like(tensor) - 0.5) * self.std
-        noisy_tensor = tensor + noise
+        noisy_tensor = tensor + noise.to(tensor.device)
         return noisy_tensor
 
 class BaseModel(nn.Module):
-    """
-    A pytorch implementation of MMoE Model.
 
-    Reference:
-        Ma, Jiaqi, et al. Modeling task relationships in multi-task learning with multi-gate mixture-of-experts. KDD 2018.
-    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if kwargs.get('aug',None):
+            if kwargs.get('aug_type',None) == 'uniform':
+                self.aug = AddUniformNoise(**kwargs)
+            else:
+                self.aug = AddGaussianNoise(**kwargs)
 
-    def __init__(self, categorical_field_dims, numerical_num, embed_dim, bottom_mlp_dims, tower_mlp_dims, task_num, expert_num, dropout):
-        NotImplemented
 
-
-    def get_shared_output(self, categorical_x, numerical_x,training=True,aug=False):
+    def get_embeding_output(self, categorical_x, numerical_x,training=True,aug=False):
         categorical_emb = self.embedding(categorical_x)
         numerical_emb = self.numerical_layer(numerical_x).unsqueeze(1)
         emb = torch.cat([categorical_emb, numerical_emb], 1).view(-1, self.embed_output_dim)
